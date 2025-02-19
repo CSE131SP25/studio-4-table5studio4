068fb2b846176e8fb65b269f97c18e8a944745b7
gpurd <gpurd@DESKTOP-3L2STKN.dhcp.wustl.edu>
2025-02-19 15:10:30 -0600

    Studio4.2

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,9 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		StdDraw.setPenColor(250,0,0);
-		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.1);
-		
+		StdDraw.setPenColor(137,207,240);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+		StdDraw.setPenColor(30,73,41);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		StdDraw.filledRectangle(0.5, 0.35, 0.3, 0.05);
+		StdDraw.filledRectangle(0.5, 0.65, 0.3, 0.05);
+		StdDraw.setPenColor(62,250,210);
+		StdDraw.filledCircle(0.3,0.5,0.1);
+		StdDraw.filledCircle(0.7,0.5,0.1);
 		
 	}
 }
