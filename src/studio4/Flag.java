7a51f09e39e15833673d65ab5c3fbb5eb208039a
gpurd <gpurd@DESKTOP-3L2STKN.dhcp.wustl.edu>
2025-02-19 15:07:09 -0600

    Studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,9 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(250,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.1);
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,38 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComp = in.nextInt();
+		int greenComp = in.nextInt();
+		int blueComp = in.nextInt();
+		String isFilled = in.next();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		if (shapeType .equals("rectangle")) {
+			StdDraw.setPenColor(redComp, greenComp, blueComp);
+			if (isFilled .equals("true")) {
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}else {
+			StdDraw.rectangle(p1, p2, p3, p4);
+			}
+		}
+		if (shapeType .equals("circle")) {
+			StdDraw.setPenColor(redComp, greenComp, blueComp);
+			if (isFilled .equals("true")) {
+				StdDraw.filledCircle(p1, p2, p3);
+			}else {
+			StdDraw.circle(p1, p2, p3);
+			}
+		}
+		if (shapeType .equals("ellipse")) {
+			StdDraw.setPenColor(redComp, greenComp, blueComp);
+			if (isFilled .equals("true")) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}else {
+			StdDraw.ellipse(p1, p2, p3, p4);
+			}
+		}
 	}
 }
